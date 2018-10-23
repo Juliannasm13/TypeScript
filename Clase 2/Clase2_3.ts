b1a1e6c1af5408de9a1520068f60b329c00cdb61
Julianna <juliannasm120@hotmail.com>
2018-10-23 12:08:10 -0430

    Clase 2 (con carpeta asesor)

---
+++ Clase 2/Clase2_3.ts
@@ -0,0 +1,49 @@
+interface Punto{
+    x:number,
+    y:number
+}
+function pintarTriangulo(p1:Punto,p2:Punto,p3:Punto,
+                        contexto:CanvasRenderingContext2D,
+                        relleno:boolean=false){
+    contexto.beginPath();
+    contexto.moveTo(p1.x,p1.y);
+    contexto.lineTo(p2.x,p2.y);
+    contexto.lineTo(p3.x,p3.y);
+    if (relleno){
+        contexto.fill()
+    }
+    contexto.closePath();
+    contexto.stroke();
+}
+function pintarCirculo(p1:Punto,radio:number,
+                        contexto:CanvasRenderingContext2D,
+                        relleno:boolean=false){
+    contexto.moveTo(p1.x,p1.y)
+    contexto.arc(p1.x,p1.y,radio,0,Math.PI*2);
+    contexto.stroke();
+}
+var c1:any = document.getElementById("canvas1");
+var c2:any = document.getElementById("canvas2");
+
+if (c1!==undefined && c1.getContext){
+    let ancho:number=c1.width;
+    let alto:number=c1.height;
+
+    var ctx1:CanvasRenderingContext2D=c1.getContext("2d");
+    var ctx2:CanvasRenderingContext2D=c2.getContext("2d");
+    for (var i=1; i<=10; i++){
+        pintarCirculo({x:i*50,y:i*50},20,ctx2,true)
+    }
+    pintarTriangulo({x:0,y:0},
+                    {x:ancho,y:alto},
+                    {x:0,y:alto},
+                    ctx1,
+                    true);
+
+    pintarTriangulo({x:50,y:50},
+                    {x:400,y:0},
+                    {x:0,y:400},
+                    ctx2,
+                    true);
+}else
+    console.log("No se pudo acceder al canvas")
